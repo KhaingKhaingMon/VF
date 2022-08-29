10edc737291a103179065dee47fbf2119f5a0028
VegetarianFalcon-Java <gambhiirii@gmail.com>
2022-08-29 13:52:10 +0630

    Just trying to understand methods.

--- src/Name.java
+++ src/Name.java
@@ -4,11 +4,16 @@ public class Name {
     }
 
     static void yourName(){
-        System.out.println ("Your name is FalconianVeggie!");
+        System.out.println ("What is your name?");
+    }
+
+    static void callMe(String name){
+        System.out.println ("Hey " + name);
     }
 
     public static void main(String[] args) {
         myName();
+        callMe("Friend");
         yourName();
     }
 }
