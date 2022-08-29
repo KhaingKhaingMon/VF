e7b7bad0edb6aba43a00779c071b18e748fb872f
VegetarianFalcon-Java <gambhiirii@gmail.com>
2022-08-29 13:34:32 +0630

    Just trying to understand method calls.

---
+++ src/Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    static void myMethod() {
+        System.out.println("My name is VegetarianFalcon!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
