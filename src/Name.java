825af79efbef2b26c6d943b76de50f4ae48f62ad
VegetarianFalcon-Java <gambhiirii@gmail.com>
2022-08-29 13:42:59 +0630

    Just trying to understand methods.

--- src/Name.java
+++ src/Name.java
@@ -1,9 +1,14 @@
 public class Name {
-    static void myMethod() {
+    static void myName() {
         System.out.println("My name is VegetarianFalcon!");
     }
 
+    static void yourName(){
+        System.out.println ("Your name is FalconianVeggie!");
+    }
+
     public static void main(String[] args) {
-        myMethod();
+        myName();
+        yourName();
     }
 }
